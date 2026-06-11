0e814c7ddff4f16d2b485600979963a3d8bbdbe8
zohreh-mousavinasab <87121330+zohreh-mousavinasab@users.noreply.github.com>
2026-06-11 14:55:43 +0200

    Set base path for Vite configuration

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig(({ mode }) => {
 
   return {
     plugins: [react()],
+    base: "/AI-Powered-Document-Intelligence-SaaS/",
     server: {
       host: "127.0.0.1",
       port: 5173,
